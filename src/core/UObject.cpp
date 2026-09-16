0d8a779f1348d05da505902f1f4a19ab9c1e35f2
abdallh-rgx <abdallhrgxdevloper@gmail.com>
2026-09-16 21:28:18 +0300

    Update UObject.cpp

--- src/core/UObject.cpp
+++ src/core/UObject.cpp
@@ -1,195 +1,65 @@
 #include "pch.h"
-#include "Utils.hpp"
 #include "UObject.hpp"
 #include "FName.hpp"
 
-#include <cstdlib>
-
 namespace Sarah {
 
-std::atomic<uint32_t> GFastArrayIDCounter{1};
+ProcessEvent_t ProcessEventPtr = nullptr;
 
-void* EngineRealloc(void* ptr, int64_t newLen, uint32_t alignment) {
-    if (alignment <= 16) {
-        if (newLen == 0) {
-            free(ptr);
-            return nullptr;
-        }
-        return realloc(ptr, (size_t)newLen);
+int32_t UObjectManager::Num() {
+    return GObjectsNum();
+}
+
+UObject* UObjectManager::GetByIndex(int32_t index) {
+    return GetObjectByIndex(index);
+}
+
+UObject* UObjectManager::Find(const wchar_t* path, UClass* cls) {
+    if (!Sarah::ImageBase) return nullptr;
+    std::u16string u16 = WToU16(path);
+    using t = UObject*(*)(UClass*, UObject*, const char16_t*, bool);
+    t fn = (t)(Sarah::ImageBase + Off::StaticFindObject);
+    return fn(cls, nullptr, u16.c_str(), false);
+}
+
+UObject* UObjectManager::Load(const wchar_t* path, UClass* cls) {
+    if (!Sarah::ImageBase) return nullptr;
+    std::u16string u16 = WToU16(path);
+    using t = UObject*(*)(UClass*, UObject*, const char16_t*, const char16_t*, uint32_t, void*, bool, void*);
+    t fn = (t)(Sarah::ImageBase + Off::StaticLoadObjectPub);
+    return fn(cls, nullptr, u16.c_str(), nullptr, 0, nullptr, false, nullptr);
+}
+
+UObject* UObjectManager::FindOrLoad(const wchar_t* path, UClass* cls) {
+    UObject* obj = Find(path, cls);
+    if (obj) return obj;
+    return Load(path, cls);
+}
+
+void CallProcessEvent(UObject* obj, UFunction* func, void* params) {
+    if (!ProcessEventPtr) {
+        ProcessEventPtr = (ProcessEvent_t)(Sarah::ImageBase + Off::ProcessEvent);
     }
-    size_t aligned = (size_t)(alignment + 15) & ~(size_t)15;
-    if (newLen == 0) {
-        free(ptr);
-        return nullptr;
-    }
-    void* original = ptr ? *((void**)ptr - 1) : nullptr;
-    if (ptr && !original) return nullptr;
-    void* raw = realloc(original, (size_t)newLen + aligned);
-    if (!raw) return nullptr;
-    uintptr_t addr = (uintptr_t)raw + aligned;
-    *((void**)addr - 1) = raw;
-    return (void*)addr;
+    if (!obj || !func) return;
+    ProcessEventPtr(obj, func, params);
 }
 
 }
 
-UObject* Utils::FindObject(const wchar_t* path, UClass* cls) {
-    return Sarah::UObjectManager::Find(path, cls);
+uintptr_t InSDKUtils::GetImageBase() {
+    return Sarah::ImageBase;
 }
 
-UObject* Utils::LoadObject(const wchar_t* path, UClass* cls) {
-    return Sarah::UObjectManager::Load(path, cls);
+uintptr_t InSDKUtils::GetGNames() {
+    return Sarah::ImageBase + Off::GNames;
 }
 
-UObject* Utils::FindOrLoad(const wchar_t* path, UClass* cls) {
-    return Sarah::UObjectManager::FindOrLoad(path, cls);
+TUObjectArray* InSDKUtils::GetGObjects() {
+    return reinterpret_cast<TUObjectArray*>(Sarah::ImageBase + Off::GObjects);
 }
 
-AActor* Utils::SpawnActor(UClass* cls, const FVector& loc, const FRotator& rot, AActor* owner) {
-    if (!cls) return nullptr;
-
-    struct FSpawnParams {
-        void* Name;
-        AActor* Template;
-        AActor* Owner;
-        void* Instigator;
-        void* OverrideLevel;
-        void* OverrideParentComponent;
-        uint8_t SpawnCollisionHandlingOverride;
-        uint8_t bRemoteOwned : 1;
-        uint8_t bNoFail : 1;
-        uint8_t bDeferConstruction : 1;
-        uint8_t bAllowDuringConstructionScript : 1;
-        uint8_t NameMode;
-        uint32_t ObjectFlags;
-    };
-
-    FSpawnParams params = {};
-    params.Owner = owner;
-    params.SpawnCollisionHandlingOverride = 1;
-    params.NameMode = 3;
-
-    CoreUObject::FTransform transform = MakeTransform(loc, rot);
-
-    using t = AActor* (*)(void*, UClass*, CoreUObject::FTransform*, FSpawnParams*);
-    static t fn = nullptr;
-    if (!fn) fn = (t)(Sarah::ImageBase + Off::UWorld_SpawnActor);
-
-    UWorld* world = UWorld::GetWorld();
-    if (!world) return nullptr;
-
-    return fn(world, cls, &transform, &params);
-}
-
-std::vector<AActor*> Utils::GetAllActors(UClass* cls) {
-    std::vector<AActor*> out;
-    if (!cls) return out;
-
-    UWorld* world = UWorld::GetWorld();
-    if (!world) return out;
-
-    TArray<AActor*> actors;
-    UGameplayStatics::GetAllActorsOfClass(world, cls, &actors);
-    for (int i = 0; i < actors.Num(); i++) out.push_back(actors[i]);
-    actors.Free();
-    return out;
-}
-
-float Utils::EvaluateScalableFloat(FScalableFloat& value) {
-    if (!value.Curve.CurveTable)
-        return value.Value;
-
-    float out = 0.f;
-    FString ctx;
-    UDataTableFunctionLibrary::EvaluateCurveTableRow(value.Curve.CurveTable, value.Curve.RowName, 0.f, nullptr, &out, ctx);
-    return out;
-}
-
-float Utils::EvaluateCurve(FCurveTableRowHandle& handle, float inTime) {
-    if (!handle.CurveTable)
-        return 0.f;
-
-    float out = 0.f;
-    FString ctx;
-    UDataTableFunctionLibrary::EvaluateCurveTableRow(handle.CurveTable, handle.RowName, inTime, nullptr, &out, ctx);
-    return out;
-}
-
-void MakeWeakPtrInto(FWeakObjectPtr& out, void* obj) {
-    out.ObjectIndex = 0;
-    out.ObjectSerialNumber = 0;
-    if (!obj || !Sarah::GObjectsLayout.Initialized) return;
-
-    int32_t n = Sarah::UObjectManager::Num();
-    for (int32_t i = 0; i < n; i++) {
-        uint8_t* item = Sarah::GetItemByIndex(i);
-        if (!item) continue;
-        if (*(void**)item == obj) {
-            out.ObjectIndex = i;
-            out.ObjectSerialNumber = *(int32_t*)(item + 0x10) * 2;
-            return;
-        }
-    }
-}
-
-FString Utils::ToFString(const std::wstring& s) {
-    std::u16string u16 = WToU16(s);
-    u16.push_back(u'\0');
-    FString result;
-    for (char16_t c : u16) result.Add(c);
-    return result;
-}
-
-std::wstring Utils::FromFString(const FString& s) {
-    return U16ToW(s.CStr(), s ? s.Num() - 1 : 0);
-}
-
-bool Utils::TagContainerHasTag(const FGameplayTagContainer& container, const wchar_t* tagName) {
-    if (container.GameplayTags.Num() == 0) return false;
-    FName tag = MakeFName(tagName);
-    if (tag.ComparisonIndex == 0) return false;
-    for (int i = 0; i < container.GameplayTags.Num(); i++) {
-        if (container.GameplayTags[i].TagName == tag)
-            return true;
-    }
-    return false;
-}
-
-bool Utils::TagContainerHasAll(const FGameplayTagContainer& container, const FGameplayTagContainer& required) {
-    if (required.GameplayTags.Num() == 0) return true;
-    for (int i = 0; i < required.GameplayTags.Num(); i++) {
-        bool found = false;
-        for (int j = 0; j < container.GameplayTags.Num(); j++) {
-            if (container.GameplayTags[j].TagName == required.GameplayTags[i].TagName) {
-                found = true;
-                break;
-            }
-        }
-        if (!found) return false;
-    }
-    return true;
-}
-
-void Utils::MarkItemDirty(FFastArraySerializer& serializer, FFastArraySerializerItem& item) {
-    int32_t& arrayKey = *(int32_t*)((uint8_t*)&serializer + 0x54);
-    arrayKey++;
-    if (item.ReplicationID == -1) {
-        item.ReplicationID = Sarah::GFastArrayIDCounter.fetch_add(1);
-    }
-    item.ReplicationKey = Sarah::GFastArrayIDCounter.fetch_add(1);
-    item.MostRecentArrayReplicationKey = arrayKey;
-}
-
-void Utils::MarkArrayDirty(FFastArraySerializer& serializer) {
-    int32_t& arrayKey = *(int32_t*)((uint8_t*)&serializer + 0x54);
-    arrayKey++;
-}
-
-FName MakeFName(const wchar_t* name) {
-    FName empty{};
-    empty.ComparisonIndex = 0;
-
-    if (!name) return empty;
+std::string InSDKUtils::GetNameByIndex(int32 Index) {
+    if (Index <= 0) return "";
 
     static UObject* lib = nullptr;
     static UFunction* convFunc = nullptr;
@@ -199,82 +69,75 @@ FName MakeFName(const wchar_t* name) {
         initialized = true;
         lib = Sarah::UObjectManager::Find(L"/Script/Engine.Default__KismetStringLibrary");
         if (lib) {
-            convFunc = (UFunction*)Sarah::UObjectManager::Find(L"/Script/Engine.KismetStringLibrary.Conv_StringToName");
+            convFunc = (UFunction*)Sarah::UObjectManager::Find(L"/Script/Engine.KismetStringLibrary.Conv_NameToString");
         }
-        LOGI("[FNAME] StringLib=%p ConvFunc=%p", lib, convFunc);
     }
 
-    if (!lib || !convFunc) return empty;
+    if (!lib || !convFunc) return "";
 
-    std::u16string u16 = WToU16(name);
-    FString fs;
-    for (char16_t c : u16) fs.Add(c);
+    FName inName{};
+    inName.ComparisonIndex = Index;
 
-    struct FConvStringToNameParams {
-        FString* InString;
-        FName    ReturnValue;
+    struct FConvNameToStringParams {
+        FName   InName;
+        FString ReturnValue;
     };
-    FConvStringToNameParams p{};
-    p.InString = &fs;
-    p.ReturnValue.ComparisonIndex = 0;
+    FConvNameToStringParams p{};
+    p.InName = inName;
 
     Sarah::CallProcessEvent(lib, convFunc, &p);
-    return p.ReturnValue;
+
+    if (p.ReturnValue.Num() == 0) return "";
+    return p.ReturnValue.ToString();
 }
 
-uint32_t MakeFNameIndex(const wchar_t* name) {
-    FName n = MakeFName(name);
-    return (uint32_t)n.ComparisonIndex;
+UObject* InSDKUtils::GetObjectByIndex(int32 Index) {
+    return Sarah::GetObjectByIndex(Index);
 }
 
-namespace SDK {
-
-UObject* FWeakObjectPtr::Get() const {
-    if (ObjectSerialNumber == 0 || ObjectIndex < 0)
-        return nullptr;
-    uint8_t* item = Sarah::GetItemByIndex(ObjectIndex);
-    if (!item)
-        return nullptr;
-    UObject* obj = *(UObject**)item;
-    if (!obj)
-        return nullptr;
-    int32_t serial = *(int32_t*)(item + 0x10) * 2;
-    if (serial != ObjectSerialNumber)
-        return nullptr;
-    return obj;
+class UClass* BasicFilesImplUtils::FindClassByName(const std::string& Name, bool bByFullName) {
+    return bByFullName ? UObject::FindClass(Name) : UObject::FindClassFast(Name);
 }
 
-bool FWeakObjectPtr::IsValid() const {
-    return Get() != nullptr;
+class UClass* BasicFilesImplUtils::FindClassByFullName(const std::string& Name) {
+    return UObject::FindClass(Name);
 }
 
-bool FWeakObjectPtr::operator==(const FWeakObjectPtr& Other) const {
-    return ObjectIndex == Other.ObjectIndex && ObjectSerialNumber == Other.ObjectSerialNumber;
+std::string BasicFilesImplUtils::GetObjectName(class UClass* Class) {
+    return Class->GetName();
 }
 
-bool FWeakObjectPtr::operator!=(const FWeakObjectPtr& Other) const {
-    return !(*this == Other);
+int32 BasicFilesImplUtils::GetObjectIndex(class UClass* Class) {
+    return Class->Index;
 }
 
-bool FWeakObjectPtr::operator==(const class UObject* Other) const {
-    return Get() == Other;
+uint64 BasicFilesImplUtils::GetObjFNameAsUInt64(class UClass* Class) {
+    return *reinterpret_cast<uint64*>(&Class->Name);
 }
 
-bool FWeakObjectPtr::operator!=(const class UObject* Other) const {
-    return Get() != Other;
+class UObject* BasicFilesImplUtils::GetObjectByIndex(int32 Index) {
+    return InSDKUtils::GetObjectByIndex(Index);
 }
 
+UFunction* BasicFilesImplUtils::FindFunctionByFName(const FName* Name) {
+    for (int i = 0; i < InSDKUtils::GetGObjects()->Num(); ++i) {
+        UObject* Object = InSDKUtils::GetObjectByIndex(i);
+        if (!Object)
+            continue;
+        if (Object->Name == *Name)
+            return static_cast<UFunction*>(Object);
+    }
+    return nullptr;
 }
 
-namespace UC {
-
-void* ContainerRealloc(void* ptr, int64 newLen, uint32 alignment) {
-    (void)alignment;
-    return std::realloc(ptr, (size_t)newLen);
+FName BasicFilesImplUtils::StringToName(const TCHAR* Name) {
+    std::u16string u16((const char16_t*)Name);
+    std::wstring wide = U16ToW((const char16_t*)Name, u16.size());
+    return MakeFName(wide.c_str());
 }
 
-void ContainerFree(void* ptr) {
-    (void)ptr;
-}
-
+class UObject* BasicFilesImplUtils::GetDefaultObjectImpl(UClass* Class) {
+    if (Class)
+        return Class->ClassDefaultObject;
+    return nullptr;
 }
