dd26c1ba0eb6f0c63616d221e0b53ac4b762ca37
abdallh-rgx <abdallhrgxdevloper@gmail.com>
2026-09-17 21:23:32 +0300

    Update GameMode.cpp

--- src/modules/GameMode.cpp
+++ src/modules/GameMode.cpp
@@ -12,6 +12,35 @@
 #include "UObject.hpp"
 #include "FName.hpp"
 
+static UFunction* g_ReadyToStartMatch = nullptr;
+static UFunction* g_HandleStartingNewPlayer = nullptr;
+static UFunction* g_OnAircraftEnteredDropZone = nullptr;
+static UFunction* g_OnAircraftExitedDropZone = nullptr;
+
+static UFunction* GetReadyToStartMatch() {
+    if (!g_ReadyToStartMatch)
+        g_ReadyToStartMatch = (UFunction*)Utils::FindObject(L"/Script/Engine.GameMode.ReadyToStartMatch");
+    return g_ReadyToStartMatch;
+}
+
+static UFunction* GetHandleStartingNewPlayer() {
+    if (!g_HandleStartingNewPlayer)
+        g_HandleStartingNewPlayer = (UFunction*)Utils::FindObject(L"/Script/Engine.GameModeBase.HandleStartingNewPlayer");
+    return g_HandleStartingNewPlayer;
+}
+
+static UFunction* GetOnAircraftEnteredDropZone() {
+    if (!g_OnAircraftEnteredDropZone)
+        g_OnAircraftEnteredDropZone = (UFunction*)Utils::FindObject(L"/Script/FortniteGame.FortGameModeAthena.OnAircraftEnteredDropZone");
+    return g_OnAircraftEnteredDropZone;
+}
+
+static UFunction* GetOnAircraftExitedDropZone() {
+    if (!g_OnAircraftExitedDropZone)
+        g_OnAircraftExitedDropZone = (UFunction*)Utils::FindObject(L"/Script/FortniteGame.FortGameModeAthena.OnAircraftExitedDropZone");
+    return g_OnAircraftExitedDropZone;
+}
+
 UFortPlaylistAthena* GameMode::GetPlaylist() {
     if (bDuos)
         return Utils::Find<UFortPlaylistAthena>(L"FortPlaylistAthena Playlist_DefaultDuo.Playlist_DefaultDuo");
@@ -57,14 +86,15 @@ void GameMode::SetPlaylist(AFortGameModeAthena* gameMode) {
 }
 
 static bool bReady = false;
+static bool bLootInitialized = false;
 
 void GameMode::ReadyToStartMatch(UObject* context, Params::AGameMode_ReadyToStartMatch* params) {
     params->ReturnValue = false;
 
     auto gameMode = CastSDK<AFortGameModeAthena>(context);
     if (!gameMode) {
-        Sarah::CallProcessEvent(context,
-            (UFunction*)Sarah::UObjectManager::Find(L"/Script/Engine.GameMode.ReadyToStartMatch"), params);
+        UFunction* pfn = GetReadyToStartMatch();
+        if (pfn) Sarah::CallProcessEvent(context, pfn, params);
         return;
     }
 
@@ -111,82 +141,87 @@ void GameMode::ReadyToStartMatch(UObject* context, Params::AGameMode_ReadyToStar
             return;
         }
 
-        gameState->DefaultParachuteDeployTraceForGroundDistance = 10000;
+        if (!bLootInitialized) {
+            bLootInitialized = true;
 
-        auto gas = Utils::Find<UFortAbilitySet>(L"/Game/Abilities/Player/Generic/Traits/DefaultPlayer/GAS_AthenaPlayer.GAS_AthenaPlayer");
-        if (gas) AbilitySets.push_back(gas);
+            gameState->DefaultParachuteDeployTraceForGroundDistance = 10000;
 
-        auto addTierData = [&](UDataTable* table) {
-            if (!table) return;
-            if (auto compositeTable = CastSDK<UCompositeDataTable>(table)) {
-                for (auto parentTable : compositeTable->ParentTables) {
-                    if (!parentTable) continue;
-                    for (auto& pair : parentTable->RowMap) {
-                        FFortLootTierData* row = (FFortLootTierData*)pair.Value();
-                        if (row) Looting::TierDataAllGroups.push_back(row);
+            auto gas = Utils::Find<UFortAbilitySet>(L"/Game/Abilities/Player/Generic/Traits/DefaultPlayer/GAS_AthenaPlayer.GAS_AthenaPlayer");
+            if (gas) AbilitySets.push_back(gas);
+
+            auto addTierData = [&](UDataTable* table) {
+                if (!table) return;
+                if (auto compositeTable = CastSDK<UCompositeDataTable>(table)) {
+                    for (auto parentTable : compositeTable->ParentTables) {
+                        if (!parentTable) continue;
+                        for (auto& pair : parentTable->RowMap) {
+                            FFortLootTierData* row = (FFortLootTierData*)pair.Value();
+                            if (row) Looting::TierDataAllGroups.push_back(row);
+                        }
                     }
                 }
-            }
-            for (auto& pair : table->RowMap) {
-                FFortLootTierData* row = (FFortLootTierData*)pair.Value();
-                if (row) Looting::TierDataAllGroups.push_back(row);
-            }
-        };
+                for (auto& pair : table->RowMap) {
+                    FFortLootTierData* row = (FFortLootTierData*)pair.Value();
+                    if (row) Looting::TierDataAllGroups.push_back(row);
+                }
+            };
 
-        auto addPackages = [&](UDataTable* table) {
-            if (!table) return;
-            if (auto compositeTable = CastSDK<UCompositeDataTable>(table)) {
-                for (auto parentTable : compositeTable->ParentTables) {
-                    if (!parentTable) continue;
-                    for (auto& pair : parentTable->RowMap) {
-                        FFortLootPackageData* row = (FFortLootPackageData*)pair.Value();
-                        if (row) Looting::LPGroupsAll.push_back(row);
+            auto addPackages = [&](UDataTable* table) {
+                if (!table) return;
+                if (auto compositeTable = CastSDK<UCompositeDataTable>(table)) {
+                    for (auto parentTable : compositeTable->ParentTables) {
+                        if (!parentTable) continue;
+                        for (auto& pair : parentTable->RowMap) {
+                            FFortLootPackageData* row = (FFortLootPackageData*)pair.Value();
+                            if (row) Looting::LPGroupsAll.push_back(row);
+                        }
                     }
                 }
+                for (auto& pair : table->RowMap) {
+                    FFortLootPackageData* row = (FFortLootPackageData*)pair.Value();
+                    if (row) Looting::LPGroupsAll.push_back(row);
+                }
+            };
+
+            UFortPlaylistAthena* playlist = GetPlaylist();
+            auto lootTierData = playlist ? playlist->LootTierData.Get() : nullptr;
+            if (!lootTierData)
+                lootTierData = Utils::Find<UDataTable>(L"/Game/Items/Datatables/AthenaLootTierData_Client.AthenaLootTierData_Client");
+            if (lootTierData)
+                addTierData(lootTierData);
+
+            auto lootPackages = playlist ? playlist->LootPackages.Get() : nullptr;
+            if (!lootPackages)
+                lootPackages = Utils::Find<UDataTable>(L"/Game/Items/Datatables/AthenaLootPackages_Client.AthenaLootPackages_Client");
+            if (lootPackages)
+                addPackages(lootPackages);
+
+            int numObjects = Sarah::UObjectManager::Num();
+            for (int i = 0; i < numObjects; i++) {
+                auto object = Sarah::UObjectManager::GetByIndex(i);
+                if (!object || !object->Class || object->IsDefaultObject())
+                    continue;
+
+                if (auto gameFeatureData = CastSDK<UFortGameFeatureData>(object)) {
+                    auto& lootTableData = gameFeatureData->DefaultLootTableData;
+
+                    std::wstring ltdPath = FNameToWString(lootTableData.LootTierData.ObjectID.AssetPathName);
+                    std::wstring abilitySetPath = FNameToWString(gameFeatureData->PlayerAbilitySet.ObjectID.AssetPathName);
+                    std::wstring lpdPath = FNameToWString(lootTableData.LootPackageData.ObjectID.AssetPathName);
+
+                    auto abilitySet = Utils::Find<UFortAbilitySet>(abilitySetPath.c_str());
+                    auto ltdFeatureData = Utils::Find<UDataTable>(ltdPath.c_str());
+                    auto lpdFeatureData = Utils::Find<UDataTable>(lpdPath.c_str());
+
+                    if (abilitySet) AbilitySets.push_back(abilitySet);
+                    if (ltdFeatureData) addTierData(ltdFeatureData);
+                    if (lpdFeatureData) addPackages(lpdFeatureData);
+                }
             }
-            for (auto& pair : table->RowMap) {
-                FFortLootPackageData* row = (FFortLootPackageData*)pair.Value();
-                if (row) Looting::LPGroupsAll.push_back(row);
-            }
-        };
 
-        UFortPlaylistAthena* playlist = GetPlaylist();
-        auto lootTierData = playlist ? playlist->LootTierData.Get() : nullptr;
-        if (!lootTierData)
-            lootTierData = Utils::Find<UDataTable>(L"/Game/Items/Datatables/AthenaLootTierData_Client.AthenaLootTierData_Client");
-        if (lootTierData)
-            addTierData(lootTierData);
-
-        auto lootPackages = playlist ? playlist->LootPackages.Get() : nullptr;
-        if (!lootPackages)
-            lootPackages = Utils::Find<UDataTable>(L"/Game/Items/Datatables/AthenaLootPackages_Client.AthenaLootPackages_Client");
-        if (lootPackages)
-            addPackages(lootPackages);
-
-        for (int i = 0; i < Sarah::UObjectManager::Num(); i++) {
-            auto object = Sarah::UObjectManager::GetByIndex(i);
-            if (!object || !object->Class || object->IsDefaultObject())
-                continue;
-
-            if (auto gameFeatureData = CastSDK<UFortGameFeatureData>(object)) {
-                auto& lootTableData = gameFeatureData->DefaultLootTableData;
-
-                std::wstring ltdPath = FNameToWString(lootTableData.LootTierData.ObjectID.AssetPathName);
-                std::wstring abilitySetPath = FNameToWString(gameFeatureData->PlayerAbilitySet.ObjectID.AssetPathName);
-                std::wstring lpdPath = FNameToWString(lootTableData.LootPackageData.ObjectID.AssetPathName);
-
-                auto abilitySet = Utils::Find<UFortAbilitySet>(abilitySetPath.c_str());
-                auto ltdFeatureData = Utils::Find<UDataTable>(ltdPath.c_str());
-                auto lpdFeatureData = Utils::Find<UDataTable>(lpdPath.c_str());
-
-                if (abilitySet) AbilitySets.push_back(abilitySet);
-                if (ltdFeatureData) addTierData(ltdFeatureData);
-                if (lpdFeatureData) addPackages(lpdFeatureData);
-            }
+            Looting::SpawnFloorLootForContainer(Utils::Find<UBlueprintGeneratedClass>(L"/Game/Athena/Environments/Blueprints/Tiered_Athena_FloorLoot_Warmup.Tiered_Athena_FloorLoot_Warmup_C"));
+            Looting::SpawnFloorLootForContainer(Utils::Find<UBlueprintGeneratedClass>(L"/Game/Athena/Environments/Blueprints/Tiered_Athena_FloorLoot_01.Tiered_Athena_FloorLoot_01_C"));
         }
-
-        Looting::SpawnFloorLootForContainer(Utils::Find<UBlueprintGeneratedClass>(L"/Game/Athena/Environments/Blueprints/Tiered_Athena_FloorLoot_Warmup.Tiered_Athena_FloorLoot_Warmup_C"));
-        Looting::SpawnFloorLootForContainer(Utils::Find<UBlueprintGeneratedClass>(L"/Game/Athena/Environments/Blueprints/Tiered_Athena_FloorLoot_01.Tiered_Athena_FloorLoot_01_C"));
 
         gameMode->bWorldIsReady = true;
         LOGI("[GameMode] World is ready");
@@ -216,6 +251,8 @@ APawn* GameMode::SpawnDefaultPawnFor(AGameModeBase* context, AController* newPla
     auto playerController = CastSDK<AFortPlayerController>(newPlayer);
     if (!playerController) return pawn;
 
+    if (!playerController->WorldInventory) return pawn;
+
     auto num = playerController->WorldInventory->Inventory.ReplicatedEntries.Num();
     if (num != 0) {
         playerController->WorldInventory->Inventory.ReplicatedEntries.ResetNum();
@@ -234,21 +271,28 @@ APawn* GameMode::SpawnDefaultPawnFor(AGameModeBase* context, AController* newPla
 
     if (num == 0) {
         auto playerState = (AFortPlayerStateAthena*)playerController->PlayerState;
-        if (playerState) {
-            for (auto& abilitySet : AbilitySets)
-                Abilities::GiveAbilitySet(playerState->AbilitySystemComponent, abilitySet);
+        if (!playerState) return pawn;
 
-            auto athenaController = (AFortPlayerControllerAthena*)playerController;
-            if (athenaController->XPComponent) {
-                playerState->SeasonLevelUIDisplay = athenaController->XPComponent->CurrentLevel;
-                playerState->OnRep_SeasonLevelUIDisplay();
+        for (auto& abilitySet : AbilitySets) {
+            if (abilitySet && playerState->AbilitySystemComponent) {
+                Abilities::GiveAbilitySet(playerState->AbilitySystemComponent, abilitySet);
             }
+        }
+
+        auto athenaController = (AFortPlayerControllerAthena*)playerController;
+        if (athenaController->XPComponent) {
+            playerState->SeasonLevelUIDisplay = athenaController->XPComponent->CurrentLevel;
+            playerState->OnRep_SeasonLevelUIDisplay();
         }
     }
 
     if (auto athenaController = CastSDK<AFortPlayerControllerAthena>(playerController)) {
         if (!athenaController->MatchReport) {
-            athenaController->MatchReport = (UAthenaPlayerMatchReport*)UGameplayStatics::SpawnObject(UAthenaPlayerMatchReport::StaticClass(), athenaController);
+            UClass* reportClass = UAthenaPlayerMatchReport::StaticClass();
+            if (reportClass) {
+                athenaController->MatchReport = (UAthenaPlayerMatchReport*)UGameplayStatics::SpawnObject(reportClass, athenaController);
+                LOGI("[GameMode] MatchReport (pawn) = %p", athenaController->MatchReport);
+            }
         }
     }
 
@@ -259,34 +303,58 @@ APawn* GameMode::SpawnDefaultPawnFor(AGameModeBase* context, AController* newPla
 void GameMode::HandleStartingNewPlayer(UObject* context, Params::AGameModeBase_HandleStartingNewPlayer* params) {
     auto gameMode = (AFortGameModeAthena*)context;
     auto newPlayer = (AFortPlayerControllerAthena*)params->NewPlayer;
-    if (!gameMode || !newPlayer) return;
+
+    UFunction* pfn = GetHandleStartingNewPlayer();
+
+    if (!gameMode || !newPlayer) {
+        if (pfn) Sarah::CallProcessEvent(context, pfn, params);
+        return;
+    }
 
     auto gameState = (AFortGameStateAthena*)gameMode->GameState;
     AFortPlayerStateAthena* playerState = (AFortPlayerStateAthena*)newPlayer->PlayerState;
-    if (!playerState || !gameState) return;
+
+    if (!playerState || !gameState) {
+        if (pfn) Sarah::CallProcessEvent(context, pfn, params);
+        return;
+    }
 
     playerState->SquadId = playerState->TeamIndex - 3;
     playerState->OnRep_SquadId();
 
+    FGameMemberInfo Member;
+    Member.MostRecentArrayReplicationKey = -1;
+    Member.ReplicationID = -1;
+    Member.ReplicationKey = -1;
+    Member.TeamIndex = playerState->TeamIndex;
+    Member.SquadId = playerState->SquadId;
+    Member.MemberUniqueId = playerState->UniqueId;
+
+    gameState->GameMemberInfoArray.Members.Add(Member);
+    gameState->GameMemberInfoArray.MarkItemDirty(Member);
+
     StartingCount++;
 
     if (!newPlayer->MatchReport) {
-        newPlayer->MatchReport = (UAthenaPlayerMatchReport*)UGameplayStatics::SpawnObject(UAthenaPlayerMatchReport::StaticClass(), newPlayer);
+        UClass* reportClass = UAthenaPlayerMatchReport::StaticClass();
+        if (reportClass) {
+            newPlayer->MatchReport = (UAthenaPlayerMatchReport*)UGameplayStatics::SpawnObject(reportClass, newPlayer);
+            LOGI("[GameMode] MatchReport (newPlayer) = %p", newPlayer->MatchReport);
+        } else {
+            LOGE("[GameMode] MatchReport StaticClass is null");
+        }
     }
 
-    Sarah::CallProcessEvent(context,
-        (UFunction*)Sarah::UObjectManager::Find(L"/Script/Engine.GameModeBase.HandleStartingNewPlayer"), params);
+    if (pfn) Sarah::CallProcessEvent(context, pfn, params);
 }
 
 void GameMode::OnAircraftEnteredDropZone(UObject* context, Params::AFortGameModeAthena_OnAircraftEnteredDropZone* params) {
-    auto gameMode = (AFortGameModeAthena*)context;
-
     if (!bDev && bGameSessions) {
         API::GameServer(BackendUrl + "/solstice/api/v1/matchmaking/stop/by-address", IP, g_Port);
     }
 
-    Sarah::CallProcessEvent(context,
-        (UFunction*)Sarah::UObjectManager::Find(L"/Script/FortniteGame.FortGameModeAthena.OnAircraftEnteredDropZone"), params);
+    UFunction* pfn = GetOnAircraftEnteredDropZone();
+    if (pfn) Sarah::CallProcessEvent(context, pfn, params);
 }
 
 void GameMode::OnAircraftExitedDropZone(UObject* context, Params::AFortGameModeAthena_OnAircraftExitedDropZone* params) {
@@ -294,7 +362,7 @@ void GameMode::OnAircraftExitedDropZone(UObject* context, Params::AFortGameModeA
     if (!gameMode) return;
 
     for (auto& player : gameMode->AlivePlayers) {
-        if (player->IsInAircraft()) {
+        if (player && player->IsInAircraft()) {
             auto aircraftComponent = player->GetAircraftComponent();
             if (aircraftComponent) {
                 FRotator rot{};
@@ -303,8 +371,8 @@ void GameMode::OnAircraftExitedDropZone(UObject* context, Params::AFortGameModeA
         }
     }
 
-    Sarah::CallProcessEvent(context,
-        (UFunction*)Sarah::UObjectManager::Find(L"/Script/FortniteGame.FortGameModeAthena.OnAircraftExitedDropZone"), params);
+    UFunction* pfn = GetOnAircraftExitedDropZone();
+    if (pfn) Sarah::CallProcessEvent(context, pfn, params);
 }
 
 EEFortTeam GameMode::PickTeam(AFortGameModeAthena* gameMode, uint8_t preferredTeam, AFortPlayerControllerAthena* controller) {
@@ -314,6 +382,8 @@ EEFortTeam GameMode::PickTeam(AFortGameModeAthena* gameMode, uint8_t preferredTe
     int maxSquad = 1;
     if (gameState && gameState->CurrentPlaylistInfo.BasePlaylist)
         maxSquad = gameState->CurrentPlaylistInfo.BasePlaylist->MaxSquadSize;
+
+    if (maxSquad <= 0) maxSquad = 1;
 
     if (++PlayersOnCurTeam >= maxSquad) {
         CurrentTeam++;
