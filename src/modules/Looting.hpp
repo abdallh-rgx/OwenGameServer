9814b3d22a670a5e784c25e5d5df5799d6387033
abdallh-rgx <abdallhrgxdevloper@gmail.com>
2026-09-19 20:54:35 +0300

    Update Looting.hpp

--- src/modules/Looting.hpp
+++ src/modules/Looting.hpp
@@ -7,13 +7,21 @@ public:
     static inline std::vector<FFortLootTierData*> TierDataAllGroups;
     static inline std::vector<FFortLootPackageData*> LPGroupsAll;
 
+    static inline void (*ServerAttemptInteractOG)(UObject*, FFrame&) = nullptr;
+    static inline void (*PickLootDropsOG)(UObject*, FFrame&, bool*) = nullptr;
+    static inline void (*K2_SpawnPickupInWorldOG)(UObject*, FFrame&, AFortPickup**) = nullptr;
+    static inline void (*SpawnItemVariantPickupInWorldOG)(UObject*, FFrame&, AFortPickup**) = nullptr;
+    static inline void (*SupplyDropSpawnPickupOG)(UObject*, FFrame&, AFortPickup**) = nullptr;
+
     static std::vector<FFortItemEntry> ChooseLootForContainer(FName tierGroup, int lootTier = -1, int worldLevel = 0);
     static void SpawnLoot(FName& tierGroup, FVector loc);
     static void SpawnFloorLootForContainer(UBlueprintGeneratedClass* containerType);
-    static void ServerAttemptInteract(UObject* context, Params::UFortControllerComponent_Interaction_ServerAttemptInteract* params);
-    static bool PickLootDrops(UObject* context, Params::UFortKismetLibrary_PickLootDrops* params);
-    static AFortPickup* K2_SpawnPickupInWorld(UObject* context, Params::UFortKismetLibrary_K2_SpawnPickupInWorld* params);
-    static AFortPickup* SpawnItemVariantPickupInWorld(UObject* context, Params::UFortKismetLibrary_SpawnItemVariantPickupInWorld* params);
-    static AFortPickup* SupplyDropSpawnPickup(UObject* context, Params::AFortAthenaSupplyDrop_SpawnPickup* params);
+
+    static void ServerAttemptInteractHook(UObject*, FFrame&);
+    static void PickLootDropsHook(UObject*, FFrame&, bool*);
+    static void K2_SpawnPickupInWorldHook(UObject*, FFrame&, AFortPickup**);
+    static void SpawnItemVariantPickupInWorldHook(UObject*, FFrame&, AFortPickup**);
+    static void SupplyDropSpawnPickupHook(UObject*, FFrame&, AFortPickup**);
+
     static void Hook();
 };
