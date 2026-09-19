81da01ea6e3fbe806403255f18b66e9faaff0276
abdallh-rgx <abdallhrgxdevloper@gmail.com>
2026-09-19 20:44:36 +0300

    Update GameMode.hpp

--- src/modules/GameMode.hpp
+++ src/modules/GameMode.hpp
@@ -8,13 +8,21 @@ public:
     static inline uint8_t CurrentTeam = 3;
     static inline uint8_t PlayersOnCurTeam = 0;
 
+    static inline void (*ReadyToStartMatchOG)(UObject*, FFrame&, bool*) = nullptr;
+    static inline void (*HandleStartingNewPlayerOG)(UObject*, FFrame&) = nullptr;
+    static inline void (*OnAircraftEnteredDropZoneOG)(UObject*, FFrame&) = nullptr;
+    static inline void (*OnAircraftExitedDropZoneOG)(UObject*, FFrame&) = nullptr;
+
     static UFortPlaylistAthena* GetPlaylist();
     static void SetPlaylist(AFortGameModeAthena* gameMode);
-    static void ReadyToStartMatch(UObject* context, Params::AGameMode_ReadyToStartMatch* params);
+
+    static void ReadyToStartMatchHook(UObject* Context, FFrame& Stack, bool* Ret);
+    static void HandleStartingNewPlayerHook(UObject* Context, FFrame& Stack);
+    static void OnAircraftEnteredDropZoneHook(UObject* Context, FFrame& Stack);
+    static void OnAircraftExitedDropZoneHook(UObject* Context, FFrame& Stack);
+
     static APawn* SpawnDefaultPawnFor(AGameModeBase* gameMode, AController* newPlayer, AActor* startSpot);
-    static void HandleStartingNewPlayer(UObject* context, Params::AGameModeBase_HandleStartingNewPlayer* params);
-    static void OnAircraftEnteredDropZone(UObject* context, Params::AFortGameModeAthena_OnAircraftEnteredDropZone* params);
-    static void OnAircraftExitedDropZone(UObject* context, Params::AFortGameModeAthena_OnAircraftExitedDropZone* params);
     static EEFortTeam PickTeam(AFortGameModeAthena* gameMode, uint8_t preferredTeam, AFortPlayerControllerAthena* controller);
+
     static void Hook();
 };
