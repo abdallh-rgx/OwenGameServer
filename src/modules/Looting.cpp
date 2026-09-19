d12935b77baba47333eea814c0b72540ae985671
abdallh-rgx <abdallhrgxdevloper@gmail.com>
2026-09-19 20:58:20 +0300

    Update Looting.cpp

--- src/modules/Looting.cpp
+++ src/modules/Looting.cpp
@@ -23,7 +23,6 @@ static void SetupLDSForPackage(std::vector<FFortItemEntry>& lootDrops, FName pac
         float totalWeight = 0.f;
         for (auto* p : map) totalWeight += p->Weight;
         float randomNumber = ((float)rand() / 32767.f) * totalWeight;
-
         for (auto* element : map) {
             float weight = element->Weight;
             if (weight == 0) continue;
@@ -54,28 +53,22 @@ static void SetupLDSForPackage(std::vector<FFortItemEntry>& lootDrops, FName pac
     for (auto& lootDrop : lootDrops) {
         if (lootDrop.ItemDefinition == itemDefinition) {
             lootDrop.Count += lootPackage->Count;
-
             int maxStack = (int)Utils::EvaluateScalableFloat(itemDefinition->MaxStackSize);
             if (lootDrop.Count > maxStack) {
                 auto ogCount = lootDrop.Count;
                 lootDrop.Count = maxStack;
-
                 if (Inventory::GetQuickbar(lootDrop.ItemDefinition) == EEFortQuickBars::Secondary) {
-                    FFortItemEntry* extra = Inventory::MakeItemEntry(itemDefinition, ogCount - maxStack,
-                        std::clamp(Inventory::GetLevel(itemDefinition->LootLevelData), itemDefinition->MinLevel, itemDefinition->MaxLevel));
+                    FFortItemEntry* extra = Inventory::MakeItemEntry(itemDefinition, ogCount - maxStack, std::clamp(Inventory::GetLevel(itemDefinition->LootLevelData), itemDefinition->MinLevel, itemDefinition->MaxLevel));
                     lootDrops.push_back(*extra);
                     delete extra;
                 }
             }
-
-            if (Inventory::GetQuickbar(lootDrop.ItemDefinition) == EEFortQuickBars::Secondary)
-                found = true;
+            if (Inventory::GetQuickbar(lootDrop.ItemDefinition) == EEFortQuickBars::Secondary) found = true;
         }
     }
 
     if (!found && lootPackage->Count > 0) {
-        FFortItemEntry* entry = Inventory::MakeItemEntry(itemDefinition, lootPackage->Count,
-            std::clamp(Inventory::GetLevel(itemDefinition->LootLevelData), itemDefinition->MinLevel, itemDefinition->MaxLevel));
+        FFortItemEntry* entry = Inventory::MakeItemEntry(itemDefinition, lootPackage->Count, std::clamp(Inventory::GetLevel(itemDefinition->LootLevelData), itemDefinition->MinLevel, itemDefinition->MaxLevel));
         lootDrops.push_back(*entry);
         delete entry;
     }
@@ -83,21 +76,18 @@ static void SetupLDSForPackage(std::vector<FFortItemEntry>& lootDrops, FName pac
 
 std::vector<FFortItemEntry> Looting::ChooseLootForContainer(FName tierGroup, int lootTier, int worldLevel) {
     std::vector<FFortItemEntry> lootDrops;
-
     auto gameState = (AFortGameStateAthena*)UWorld::GetWorld()->GameState;
     if (worldLevel == 0 && gameState) worldLevel = gameState->WorldLevel;
 
     std::vector<FFortLootTierData*> tierDataGroups;
     for (auto const& val : TierDataAllGroups) {
-        if (val->TierGroup == tierGroup && (lootTier == -1 ? true : lootTier == val->LootTier))
-            tierDataGroups.push_back(val);
+        if (val->TierGroup == tierGroup && (lootTier == -1 ? true : lootTier == val->LootTier)) tierDataGroups.push_back(val);
     }
 
     auto pickWeightedTier = [](std::vector<FFortLootTierData*>& map) -> FFortLootTierData* {
         float totalWeight = 0.f;
         for (auto* p : map) totalWeight += p->Weight;
         float randomNumber = ((float)rand() / 32767.f) * totalWeight;
-
         for (auto* element : map) {
             float weight = element->Weight;
             if (weight == 0) continue;
@@ -113,17 +103,14 @@ std::vector<FFortItemEntry> Looting::ChooseLootForContainer(FName tierGroup, int
     float dropCount = 0;
     if (lootTierData->NumLootPackageDrops > 0) {
         dropCount = lootTierData->NumLootPackageDrops < 1 ? 1 : (float)((int)((lootTierData->NumLootPackageDrops * 2) - .5f) >> 1);
-
         if (lootTierData->NumLootPackageDrops > 1) {
             float idk = lootTierData->NumLootPackageDrops - dropCount;
-            if (idk > 0.0000099999997f)
-                dropCount += idk >= ((float)rand() / 32767);
+            if (idk > 0.0000099999997f) dropCount += idk >= ((float)rand() / 32767);
         }
     }
 
     float amountOfLootDrops = 0;
-    for (auto& min : lootTierData->LootPackageCategoryMinArray)
-        amountOfLootDrops += min;
+    for (auto& min : lootTierData->LootPackageCategoryMinArray) amountOfLootDrops += min;
 
     int sumWeights = 0;
     for (int i = 0; i < lootTierData->LootPackageCategoryWeightArray.Num(); ++i)
@@ -132,8 +119,7 @@ std::vector<FFortItemEntry> Looting::ChooseLootForContainer(FName tierGroup, int
 
     while (sumWeights > 0) {
         amountOfLootDrops++;
-        if (amountOfLootDrops >= lootTierData->NumLootPackageDrops)
-            break;
+        if (amountOfLootDrops >= lootTierData->NumLootPackageDrops) break;
         sumWeights--;
     }
 
@@ -151,19 +137,10 @@ std::vector<FFortItemEntry> Looting::ChooseLootForContainer(FName tierGroup, int
         int i = 0;
         bool hasAmmoEntry = false;
         for (auto& entry : lootDrops) {
-            if (ammoMap[ammoDefinition] > 0 && i < ammoMap[ammoDefinition]) {
-                i++;
-                continue;
-            }
-            if (entry.ItemDefinition == ammoDefinition) {
-                hasAmmoEntry = true;
-                ammoMap[ammoDefinition]++;
-                break;
-            }
+            if (ammoMap[ammoDefinition] > 0 && i < ammoMap[ammoDefinition]) { i++; continue; }
+            if (entry.ItemDefinition == ammoDefinition) { hasAmmoEntry = true; ammoMap[ammoDefinition]++; break; }
         }
-
-        if (hasAmmoEntry)
-            continue;
+        if (hasAmmoEntry) continue;
 
         FFortLootPackageData* group = nullptr;
         static auto ammoSmall = MakeFName(L"WorldList.AthenaAmmoSmall");
@@ -175,7 +152,6 @@ std::vector<FFortItemEntry> Looting::ChooseLootForContainer(FName tierGroup, int
                 break;
             }
         }
-
         if (group) {
             FFortItemEntry* entry = Inventory::MakeItemEntry(ammoDefinition, group->Count, 0);
             lootDrops.push_back(*entry);
@@ -189,16 +165,11 @@ std::vector<FFortItemEntry> Looting::ChooseLootForContainer(FName tierGroup, int
 void Looting::SpawnLoot(FName& tierGroup, FVector loc) {
     auto gameMode = (AFortGameModeAthena*)UWorld::GetWorld()->AuthorityGameMode;
     FName realTierGroup = tierGroup;
-
     if (gameMode) {
         for (auto& pair : gameMode->RedirectAthenaLootTierGroups) {
-            if (pair.Key() == tierGroup) {
-                realTierGroup = pair.Value();
-                break;
-            }
+            if (pair.Key() == tierGroup) { realTierGroup = pair.Value(); break; }
         }
     }
-
     for (auto& lootDrop : ChooseLootForContainer(realTierGroup)) {
         Inventory::SpawnPickup(loc, lootDrop);
     }
@@ -206,24 +177,27 @@ void Looting::SpawnLoot(FName& tierGroup, FVector loc) {
 
 void Looting::SpawnFloorLootForContainer(UBlueprintGeneratedClass* containerType) {
     if (!containerType) return;
-
     auto containers = Utils::GetAll<ABuildingContainer>(containerType);
     for (auto& container : containers) {
-        if (container)
-            container->BP_SpawnLoot(nullptr);
+        if (container) container->BP_SpawnLoot(nullptr);
     }
 }
 
-void Looting::ServerAttemptInteract(UObject* context, Params::UFortControllerComponent_Interaction_ServerAttemptInteract* params) {
-    auto component = (UFortControllerComponent_Interaction*)context;
-    auto pc = CastSDK<AFortPlayerController>(component->GetOwner());
+void Looting::ServerAttemptInteractHook(UObject* Context, FFrame& Stack) {
+    STACK_SAVE(Stack, _saved);
+    UObject* ReceivingActor_Obj = nullptr;
+    UObject* InteractComponent = nullptr;
+    UObject* InteractionType = nullptr;
+    Stack.StepCompiledIn(&ReceivingActor_Obj);
+    Stack.StepCompiledIn(&InteractComponent);
+    Stack.StepCompiledIn(&InteractionType);
+    Stack.IncrementCode();
 
-    auto container = CastSDK<ABuildingContainer>(params->ReceivingActor);
+    auto container = CastSDK<ABuildingContainer>(ReceivingActor_Obj);
     if (container) {
         if (!container->bAlreadySearched) {
             SpawnLoot(container->SearchLootTierGroup,
                 container->K2_GetActorLocation() + container->GetActorRightVector() * 70.f + FVector(0, 0, 50));
-
             container->bAlreadySearched = true;
             container->OnRep_bAlreadySearched();
             container->SearchBounceData.SearchAnimationCount++;
@@ -232,47 +206,106 @@ void Looting::ServerAttemptInteract(UObject* context, Params::UFortControllerCom
         return;
     }
 
-    Sarah::CallProcessEvent(context, (UFunction*)Sarah::UObjectManager::Find(L"/Script/FortniteGame.FortControllerComponent_Interaction.ServerAttemptInteract"), params);
+    CALL_OG_VOID(Stack, Context, ServerAttemptInteractOG, _saved);
 }
 
-bool Looting::PickLootDrops(UObject* context, Params::UFortKismetLibrary_PickLootDrops* params) {
-    auto lootDrops = ChooseLootForContainer(params->TierGroupName, params->ForcedLootTier, params->WorldLevel);
+void Looting::PickLootDropsHook(UObject* Context, FFrame& Stack, bool* Ret) {
+    STACK_SAVE(Stack, _saved);
+    UObject* WorldContextObject = nullptr;
+    FName TierGroupName;
+    int32 WorldLevel = 0;
+    int32 ForcedLootTier = 0;
+    Stack.StepCompiledIn(&WorldContextObject);
+    auto& OutLootToDrop = Stack.StepCompiledInRef<TArray<FFortItemEntry>>();
+    Stack.StepCompiledIn(&TierGroupName);
+    Stack.StepCompiledIn(&WorldLevel);
+    Stack.StepCompiledIn(&ForcedLootTier);
+    Stack.IncrementCode();
 
-    for (auto& lootDrop : lootDrops) {
-        params->OutLootToDrop.AddGrow(lootDrop);
-    }
+    auto LootDrops = ChooseLootForContainer(TierGroupName, ForcedLootTier, WorldLevel);
+    for (auto& ld : LootDrops) OutLootToDrop.Add(ld);
+    if (Ret) *Ret = true;
 
-    params->ReturnValue = true;
-    return true;
+    CALL_OG_RET(Stack, Context, PickLootDropsOG, _saved, Ret);
 }
 
-AFortPickup* Looting::K2_SpawnPickupInWorld(UObject* context, Params::UFortKismetLibrary_K2_SpawnPickupInWorld* params) {
+void Looting::K2_SpawnPickupInWorldHook(UObject* Context, FFrame& Stack, AFortPickup** Ret) {
+    STACK_SAVE(Stack, _saved);
+    UObject* WorldContextObject = nullptr;
+    UFortWorldItemDefinition* ItemDefinition = nullptr;
+    int32 NumberToSpawn = 0;
+    FVector Position, Direction;
+    int32 OverrideMaxStackCount = 0;
+    bool bToss = false, bRandomRotation = false, bBlockedFromAutoPickup = false;
+    int32 PickupInstigatorHandle = 0;
+    EFortPickupSourceTypeFlag SourceType{};
+    EFortPickupSpawnSource Source{};
+    AFortPlayerController* OptionalOwnerPC = nullptr;
+    bool bPickupOnlyRelevantToOwner = false;
+    Stack.StepCompiledIn(&WorldContextObject);
+    Stack.StepCompiledIn(&ItemDefinition);
+    Stack.StepCompiledIn(&NumberToSpawn);
+    Stack.StepCompiledIn(&Position);
+    Stack.StepCompiledIn(&Direction);
+    Stack.StepCompiledIn(&OverrideMaxStackCount);
+    Stack.StepCompiledIn(&bToss);
+    Stack.StepCompiledIn(&bRandomRotation);
+    Stack.StepCompiledIn(&bBlockedFromAutoPickup);
+    Stack.StepCompiledIn(&PickupInstigatorHandle);
+    Stack.StepCompiledIn(&SourceType);
+    Stack.StepCompiledIn(&Source);
+    Stack.StepCompiledIn(&OptionalOwnerPC);
+    Stack.StepCompiledIn(&bPickupOnlyRelevantToOwner);
+    Stack.IncrementCode();
+
     int loadedAmmo = 0;
-    if (auto weaponDef = CastSDK<UFortWeaponItemDefinition>(params->ItemDefinition)) {
+    if (auto weaponDef = CastSDK<UFortWeaponItemDefinition>(ItemDefinition)) {
         auto stats = Inventory::GetStats(weaponDef);
         loadedAmmo = stats ? stats->ClipSize : 0;
     }
+    if (Ret) *Ret = Inventory::SpawnPickup(Position, ItemDefinition, NumberToSpawn, loadedAmmo, SourceType, Source, OptionalOwnerPC ? OptionalOwnerPC->MyFortPawn : nullptr, bToss, bRandomRotation);
 
-    return Inventory::SpawnPickup(params->Position, params->ItemDefinition, params->NumberToSpawn, loadedAmmo,
-        params->SourceType, params->Source,
-        params->OptionalOwnerPC ? params->OptionalOwnerPC->MyFortPawn : nullptr, params->bToss, params->bRandomRotation);
+    CALL_OG_RET(Stack, Context, K2_SpawnPickupInWorldOG, _saved, Ret);
 }
 
-AFortPickup* Looting::SpawnItemVariantPickupInWorld(UObject* context, Params::UFortKismetLibrary_SpawnItemVariantPickupInWorld* params) {
-    return Inventory::SpawnPickup(params->Params_0.Position, params->Params_0.WorldItemDefinition, params->Params_0.NumberToSpawn, 0,
-        params->Params_0.SourceType, params->Params_0.Source, nullptr, params->Params_0.bToss, params->Params_0.bRandomRotation);
+void Looting::SpawnItemVariantPickupInWorldHook(UObject* Context, FFrame& Stack, AFortPickup** Ret) {
+    STACK_SAVE(Stack, _saved);
+    UObject* WorldContextObject = nullptr;
+    FSpawnItemVariantParams Params;
+    Stack.StepCompiledIn(&WorldContextObject);
+    Stack.StepCompiledIn(&Params);
+    Stack.IncrementCode();
+
+    if (Ret) *Ret = Inventory::SpawnPickup(Params.Position, Params.WorldItemDefinition, Params.NumberToSpawn, 0, Params.SourceType, Params.Source, nullptr, Params.bToss, Params.bRandomRotation);
+
+    CALL_OG_RET(Stack, Context, SpawnItemVariantPickupInWorldOG, _saved, Ret);
 }
 
-AFortPickup* Looting::SupplyDropSpawnPickup(UObject* context, Params::AFortAthenaSupplyDrop_SpawnPickup* params) {
+void Looting::SupplyDropSpawnPickupHook(UObject* Context, FFrame& Stack, AFortPickup** Ret) {
+    STACK_SAVE(Stack, _saved);
+    UFortWorldItemDefinition* ItemDefinition = nullptr;
+    int32 NumberToSpawn = 0;
+    FVector Position, Direction;
+    Stack.StepCompiledIn(&ItemDefinition);
+    Stack.StepCompiledIn(&NumberToSpawn);
+    Stack.StepCompiledIn(&Position);
+    Stack.StepCompiledIn(&Direction);
+    Stack.IncrementCode();
+
     int loadedAmmo = 0;
-    if (auto weaponDef = CastSDK<UFortWeaponItemDefinition>(params->ItemDefinition)) {
+    if (auto weaponDef = CastSDK<UFortWeaponItemDefinition>(ItemDefinition)) {
         auto stats = Inventory::GetStats(weaponDef);
         loadedAmmo = stats ? stats->ClipSize : 0;
     }
+    if (Ret) *Ret = Inventory::SpawnPickup(Position, ItemDefinition, NumberToSpawn, loadedAmmo, EEFortPickupSourceTypeFlag::Other, EEFortPickupSpawnSource::SupplyDrop);
 
-    return Inventory::SpawnPickup(params->Position, params->ItemDefinition, params->NumberToSpawn, loadedAmmo,
-        EEFortPickupSourceTypeFlag::Other, EEFortPickupSpawnSource::SupplyDrop);
+    CALL_OG_RET(Stack, Context, SupplyDropSpawnPickupOG, _saved, Ret);
 }
 
 void Looting::Hook() {
+    Utils::ExecHook(L"/Script/FortniteGame.FortControllerComponent_Interaction.ServerAttemptInteract", (void*)ServerAttemptInteractHook, ServerAttemptInteractOG);
+    Utils::ExecHook(L"/Script/FortniteGame.FortKismetLibrary.PickLootDrops", (void*)PickLootDropsHook, PickLootDropsOG);
+    Utils::ExecHook(L"/Script/FortniteGame.FortKismetLibrary.K2_SpawnPickupInWorld", (void*)K2_SpawnPickupInWorldHook, K2_SpawnPickupInWorldOG);
+    Utils::ExecHook(L"/Script/FortniteGame.FortKismetLibrary.SpawnItemVariantPickupInWorld", (void*)SpawnItemVariantPickupInWorldHook, SpawnItemVariantPickupInWorldOG);
+    Utils::ExecHook(L"/Script/FortniteGame.FortAthenaSupplyDrop.SpawnPickup", (void*)SupplyDropSpawnPickupHook, SupplyDropSpawnPickupOG);
 }
